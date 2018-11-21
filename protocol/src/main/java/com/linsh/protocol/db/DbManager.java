ba1dd531189c9d885697154356fef516c24534aa
Senh Linsh <lyuanxiang0812@126.com>
2018-11-21 11:45:46 +0800

    feat: 新增部分数据库接口

--- protocol/src/main/java/com/linsh/protocol/db/DbManager.java
+++ protocol/src/main/java/com/linsh/protocol/db/DbManager.java
@@ -1,5 +1,10 @@
 package com.linsh.protocol.db;
 
+import com.linsh.protocol.Result;
+import com.linsh.protocol.TaskHolder;
+
+import java.util.List;
+
 /**
  * <pre>
  *    author : Senh Linsh
@@ -14,4 +19,24 @@ public interface DbManager {
      * 管理配置数据
      */
     DbConfigManager config();
+
+    <T> QueryBuilder<T> query(Class<T> clazz);
+
+    <T> TaskHolder<List<T>> queryAll(Class<T> clazz);
+
+    TaskHolder<Result> update(Object entity);
+
+    TaskHolder<Result> delete(Object entity);
+
+    TaskHolder<Result> deleteAll(List<?> entity);
+
+    TaskHolder<Result> deleteAll(Class<?> clazz);
+
+    TaskHolder<Result> insert(Object entity);
+
+    TaskHolder<Result> insert(List<Object> entity);
+
+    TaskHolder<Result> insertOrUpdate(Object entity);
+
+    TaskHolder<Result> insertOrUpdate(List<Object> entity);
 }
---
+++ protocol/src/main/java/com/linsh/protocol/db/QueryBuilder.java
@@ -0,0 +1,88 @@
+package com.linsh.protocol.db;
+
+import com.linsh.protocol.TaskHolder;
+
+import java.util.List;
+
+/**
+ * <pre>
+ *    author : Senh Linsh
+ *    github : https://github.com/SenhLinsh
+ *    date   : 2018/11/20
+ *    desc   :
+ * </pre>
+ */
+public interface QueryBuilder<T> {
+
+    QueryBuilder equalTo(String fieldName, Object value);
+
+    QueryBuilder notEqualTo(String fieldName, Object value);
+
+    QueryBuilder in(String fieldName, Object... values);
+
+    <R extends Number> QueryBuilder greaterThan(String fieldName, R value);
+
+    <R extends Number> QueryBuilder greaterThanOrEqualTo(String fieldName, R value);
+
+    <R extends Number> QueryBuilder lessThan(String fieldName, R value);
+
+    <R extends Number> QueryBuilder lessThanOrEqualTo(String fieldName, R value);
+
+    <R extends Number> QueryBuilder between(String fieldName, R from, R to);
+
+    QueryBuilder contains(String fieldName, String value);
+
+    QueryBuilder beginsWith(String fieldName, String value);
+
+    QueryBuilder beginsWith(String fieldName, String value, boolean isCaseSensitive);
+
+    QueryBuilder endsWith(String fieldName, String value);
+
+    QueryBuilder endsWith(String fieldName, String value, boolean isCaseSensitive);
+
+    QueryBuilder like(String fieldName, String value);
+
+    QueryBuilder like(String fieldName, String value, boolean isCaseSensitive);
+
+    QueryBuilder distinct(String fieldName);
+
+    QueryBuilder isNull(String fieldName);
+
+    QueryBuilder isNotNull(String fieldName);
+
+    QueryBuilder isEmpty(String fieldName);
+
+    QueryBuilder isNotEmpty(String fieldName);
+
+    QueryBuilder sortAsc(String fieldName);
+
+    QueryBuilder sortAsc(String fieldName1, String fieldName2);
+
+    QueryBuilder sortDesc(String fieldName);
+
+    QueryBuilder sortDesc(String fieldName1, String fieldName2);
+
+    QueryBuilder not();
+
+    QueryBuilder and();
+
+    QueryBuilder or();
+
+    QueryBuilder beginGroup();
+
+    QueryBuilder endGroup();
+
+    TaskHolder<Long> count();
+
+    <R extends Number> TaskHolder<R> max(String fieldName);
+
+    <R extends Number> TaskHolder<R> min(String fieldName);
+
+    <R extends Number> TaskHolder<R> sum(String fieldName);
+
+    TaskHolder<Double> average(String fieldName);
+
+    TaskHolder<T> findFirst();
+
+    TaskHolder<List<T>> findAll();
+}
